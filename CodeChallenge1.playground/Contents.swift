4c736646b2e3e65e29791d8bff30d36ad208d190
Douglas Patterson <douglaspatterson801@gmail.com>
2019-05-15 10:53:09 -0600

    code challenge completed

--- CodeChallenge1.playground/Contents.swift
+++ CodeChallenge1.playground/Contents.swift
@@ -1,3 +1,29 @@
 import UIKit
 
-var str = "Hello, playground"
+var numbersDivisibleByThreeAndFive: [Int] = []
+
+let mountainland = "Mountainland"
+let technical = "Technical"
+let mountainlandTechnicalCollege = "Mountainland Technical College"
+
+for i in 1...100 {
+    let hasNoRemainder = 0
+    if i % 3 == hasNoRemainder && i % 5 == hasNoRemainder {
+        print("\(mountainlandTechnicalCollege)")
+    } else if i % 3 == hasNoRemainder {
+        print("\(mountainland)")
+        numbersDivisibleByThreeAndFive.append(i)
+    } else if i % 5 == hasNoRemainder {
+        print("\(technical)")
+        numbersDivisibleByThreeAndFive.append(i)
+    } else { print("\(i)")
+    }
+}
+
+print(numbersDivisibleByThreeAndFive)
+
+
+
+
+
+
